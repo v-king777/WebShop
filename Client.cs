2d303c55985d81a4fbf0c1872a641b3f6553edd9
v-king777 <v.kolpashchikov@gmail.com>
2021-02-13 13:22:03 +0300

    modified:   Client.cs

--- Client.cs
+++ Client.cs
@@ -6,5 +6,17 @@ namespace WebShop
 {
     class Client
     {
+        public Guid Id;
+        public string Name;
+        public string PhoneNumber;
+        public string Address;
+
+        public void Print()
+        {
+            Console.WriteLine("Id: " + Id);
+            Console.WriteLine("Name: " + Name);
+            Console.WriteLine("PhoneNumber: " + PhoneNumber);
+            Console.WriteLine("Address: " + Address);
+        }
     }
 }
