ab1e5917cd91616c9abd0766d586d0da1a601fe7
v-king777 <v.kolpashchikov@gmail.com>
2021-02-13 20:15:40 +0300

    modified:   Client.cs

--- Client.cs
+++ Client.cs
@@ -6,17 +6,55 @@ namespace WebShop
 {
     class Client
     {
-        public Guid Id;
-        public string Name;
-        public string PhoneNumber;
-        public string Address;
+        public Guid id;
+        private string name;
+        private string phoneNumber;
+        private string address;
+        private DateTime birthday;
+
+        public Client(string name, string phoneNumber, string address)
+        {
+            if (name == "" || name.Contains(" "))
+            {
+                throw new Exception("Invalid name");
+            }
+
+            if (phoneNumber == "" || phoneNumber.Contains(" "))
+            {
+                throw new Exception("Invalid phoneNumber");
+            }
+
+            if (address == "" || address.Contains(" "))
+            {
+                throw new Exception("Invalid address");
+            }
+
+            this.name = name;
+            this.phoneNumber = phoneNumber;
+            this.address = address;
+        }
+
+        public void SetBirthday(DateTime birthday)
+        {
+            if (birthday > DateTime.Now)
+            {
+                throw new Exception("Invalid birthday");
+            }
+            this.birthday = birthday;
+        }
+
+        public DateTime GetBirthday()
+        {
+            return birthday;
+        }
 
         public void Print()
         {
-            Console.WriteLine("Id: " + Id);
-            Console.WriteLine("Name: " + Name);
-            Console.WriteLine("PhoneNumber: " + PhoneNumber);
-            Console.WriteLine("Address: " + Address);
+            Console.WriteLine("Id: " + id);
+            Console.WriteLine("Name: " + name);
+            Console.WriteLine("PhoneNumber: " + phoneNumber);
+            Console.WriteLine("Address: " + address);
+            Console.WriteLine("Birthday: " + birthday);
         }
     }
 }
