07d294cf1c16c82de47b0ab6874ee36a956dce98
v-king777 <v.kolpashchikov@gmail.com>
2021-02-13 20:15:55 +0300

    modified:   Program.cs

--- Program.cs
+++ Program.cs
@@ -8,23 +8,15 @@ namespace WebShop
         {
             Console.WriteLine("\t=== WebShop ===\n");
 
-            var client_1 = GetClient();
-            client_1.Print();
-
-            Console.WriteLine();
-
-            var client_2 = new Client();
-            client_2.Print();
+            var client1 = GetClient();
+            client1.SetBirthday(DateTime.Parse("18.02.1983"));
+            client1.Print();
         }
 
         static Client GetClient()
         {
-            var client = new Client();
-
-            client.Id = Guid.NewGuid();
-            client.Name = "Vladimir";
-            client.PhoneNumber = "+7(900)528-96-08";
-            client.Address = "Kirovo-Chepetsk";
+            var client = new Client("Vladimir", "+7(900)528-96-08", "Kirovo-Chepetsk");
+            client.id = Guid.NewGuid();
 
             return client;
         }
