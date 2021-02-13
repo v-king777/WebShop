13d2591fd0b13b5aef7db7b809cf4332644e86b6
v-king777 <v.kolpashchikov@gmail.com>
2021-02-13 10:50:01 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace WebShop
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
