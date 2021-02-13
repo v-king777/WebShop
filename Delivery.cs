ed03d089f2100a3e80ee4f35c0ac0ce502291203
v-king777 <v.kolpashchikov@gmail.com>
2021-02-14 02:00:19 +0300

    new file:   Delivery.cs

---
+++ Delivery.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WebShop
+{
+    abstract class Delivery
+    {
+        public string Address { get; set; }
+        public virtual void Run()
+        {
+            Console.Write("Выполняется доставка ");
+        }
+    }
+
+    class HomeDelivery : Delivery
+    {
+        public override void Run()
+        {
+            base.Run();
+            Console.WriteLine("на дом по адресу: " + Address);
+        }
+    }
+
+    class PickPointDelivery : Delivery
+    {
+        public override void Run()
+        {
+            base.Run();
+            Console.WriteLine("в пункт выдачи по адресу: " + Address);
+        }
+    }
+
+    class ShopDelivery : Delivery
+    {
+        public override void Run()
+        {
+            base.Run();
+            Console.WriteLine("в магазин по адресу: " + Address);
+        }
+    }
+}
