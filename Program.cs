ecfca37a325e190b3d7bf94ee13c3b3cf1911787
v-king777 <v.kolpashchikov@gmail.com>
2021-02-13 13:22:34 +0300

    modified:   Program.cs

--- Program.cs
+++ Program.cs
@@ -6,7 +6,27 @@ namespace WebShop
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("\t=== WebShop ===\n");
+
+            var client_1 = GetClient();
+            client_1.Print();
+
+            Console.WriteLine();
+
+            var client_2 = new Client();
+            client_2.Print();
+        }
+
+        static Client GetClient()
+        {
+            var client = new Client();
+
+            client.Id = Guid.NewGuid();
+            client.Name = "Vladimir";
+            client.PhoneNumber = "+7(900)528-96-08";
+            client.Address = "Kirovo-Chepetsk";
+
+            return client;
         }
     }
 }
