aa042bcf01e20b9eb9081e8b3eccc3e9e36c8456
v-king777 <v.kolpashchikov@gmail.com>
2021-02-14 14:25:11 +0300

    new file:   Product.cs

---
+++ Product.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WebShop
+{
+    class Product
+    {
+        private string name;
+        private int price;
+        private string description;
+
+        public Product(string name, int price, string description)
+        {
+            if (name == "" || name.Contains(" "))
+            {
+                Console.WriteLine("Invalid name");
+            }
+            else if (price <= 0)
+            {
+                Console.WriteLine("Invalid price");
+            }
+            else if (description == "")
+            {
+                Console.WriteLine("No description");
+            }
+            else
+            {
+                this.name = name;
+                this.price = price;
+                this.description = description;
+            }
+        }
+
+        public string Description
+        {
+            get { return description; }
+        }
+
+        public int Price
+        {
+            get { return price; }
+        }
+
+        public string Name
+        {
+            get { return name; }
+        }
+
+    }
+}
