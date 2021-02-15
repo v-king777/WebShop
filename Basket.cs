11c3d96b6eb25ed62579ce35c6333a09d4c7ed43
v-king777 <v.kolpashchikov@gmail.com>
2021-02-16 00:13:56 +0300

    modified:   Basket.cs

--- Basket.cs
+++ Basket.cs
@@ -7,15 +7,16 @@ namespace WebShop
     class Basket
     {
         private Product[] products;
+        private object[] content = new object[byte.MaxValue];
 
         public Basket(Product[] products)
         {
             this.products = products;
         }
 
-        public void ProductAdd()
+        public void ProductAdd(int indexOfProduct)
         {
-            Console.WriteLine();
+            content[0] = products[indexOfProduct];
         }
     }
 }
