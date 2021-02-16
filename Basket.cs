047dec7b33b457de4f7287daff7dbb3f0e973d1e
v-king777 <77056113+v-king777@users.noreply.github.com>
2021-02-16 10:49:37 +0300

    Update Basket.cs

--- Basket.cs
+++ Basket.cs
@@ -6,9 +6,9 @@ namespace WebShop
 {
     class Basket
     {
-        private Product[] products;
         private object[] content = new object[byte.MaxValue];
-
+        private Product[] products;
+        
         public Basket(Product[] products)
         {
             this.products = products;
@@ -16,7 +16,32 @@ namespace WebShop
 
         public void ProductAdd(int indexOfProduct)
         {
-            content[0] = products[indexOfProduct];
+            for (int i = 0; i < content.Length; i++)
+            {
+                if (content[i] == null)
+                {
+                    content[i] = products[indexOfProduct];
+                    break;
+                }
+            }
+        }
+        
+        public void ViewBasket()
+        {
+            Console.WriteLine("\n\t< Корзина >\n");
+            
+            double summPrice = 0;
+            
+            foreach (Product product in content)
+            {
+                if (product != null)
+                {
+                    Console.WriteLine("{0}. Цена {1} руб.", product.Name, product.Price);
+                    summPrice = summPrice + product.Price;
+                }
+            }
+            
+            Console.WriteLine("\nСумма к оплате: {0} руб.", summPrice);
         }
     }
 }
