ca4d841ffac73f6f56af91dc441749427f1700ea
v-king777 <v.kolpashchikov@gmail.com>
2021-02-16 22:24:06 +0300

    modified:   RetailShop.cs

--- RetailShop.cs
+++ RetailShop.cs
@@ -6,13 +6,13 @@ namespace WebShop
 {
     class RetailShop : PickPoint
     {
-        public RetailShop(string name, string address): base(name, address) {}
-        
+        public RetailShop(string name, string address) : base(name, address) { }
+
         public new void Print()
         {
             Console.WriteLine("\n\t< Информация о магазине >\n");
-            Console.WriteLine("Name: " + name);
-            Console.WriteLine("Address: " + address);
+            Console.WriteLine("Название: " + name);
+            Console.WriteLine("Адрес: " + address);
         }
     }
 }
