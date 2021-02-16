f352f84601705da96c0e7a6b1c757ba4458ec3e4
v-king777 <v.kolpashchikov@gmail.com>
2021-02-16 22:23:00 +0300

    modified:   Order.cs

--- Order.cs
+++ Order.cs
@@ -4,30 +4,44 @@ using System.Text;
 
 namespace WebShop
 {
-    class Order<T1, T2>
+    class Order
     {
-        private T1 number;
-        private T2 content;
-        
-        public Order(T1 number, T2 content)
+        private string number;
+        private object[] content;
+        private double summ;
+        private string clientName;
+        private string clientPhone;
+        private string deliveryAddress;
+
+        public Order(string number, object[] content, double summ, string clientName, string clientPhone, string deliveryAddress)
         {
             this.number = number;
             this.content = content;
+            this.summ = summ;
+            this.clientName = clientName;
+            this.clientPhone = clientPhone;
+            this.deliveryAddress = deliveryAddress;
         }
-        
-        public T1 Number
-        {
-            get { return number; }
-        }
-        
+
         public void Print()
         {
             Console.WriteLine("\n\t< Информация о заказе >\n");
             Console.WriteLine("Номер заказа: " + number);
             Console.WriteLine("Дата и время заказа: " + DateTime.Now);
-            Console.WriteLine("Содержание заказа: " + content);
-            Console.WriteLine("Информация о клиенте: ");
-            Console.WriteLine("Информация о доставке: ");
+
+            Console.WriteLine("\nСписок товаров:\n");
+
+            foreach (Product product in content)
+            {
+                if (product != null)
+                {
+                    Console.WriteLine("{0}. Цена {1} руб. {2}", product.Name, product.Price, product.Description);
+                }
+            }
+
+            Console.WriteLine("\nИтого к оплате: {0} руб.", summ);
+            Console.WriteLine("\nПокупатель: {0}, тел. {1}", clientName, clientPhone);
+            Console.WriteLine("\nДоставка по адресу: " + deliveryAddress);
         }
     }
 }
