de72f3e03d377e4d5855e3546c50de3eb3bd4984
v-king777 <v.kolpashchikov@gmail.com>
2021-02-16 22:22:44 +0300

    new file:   HomeDelivery.cs

---
+++ HomeDelivery.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WebShop
+{
+    class HomeDelivery : Delivery
+    {
+        public HomeDelivery(string address) : base(address) { }
+
+        public override void DisplayAddress()
+        {
+            base.DisplayAddress();
+            Console.WriteLine("на дом по адресу: " + Address);
+        }
+    }
+}
