db1835cac76f35474f7a32c635ecf3a31448f05d
v-king777 <v.kolpashchikov@gmail.com>
2021-02-16 22:23:28 +0300

    new file:   PickPointDelivery.cs

---
+++ PickPointDelivery.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WebShop
+{
+    class PickPointDelivery : Delivery
+    {
+        public PickPointDelivery(string address) : base(address) { }
+
+        public override void DisplayAddress()
+        {
+            base.DisplayAddress();
+            Console.WriteLine("в пункт выдачи по адресу: " + Address);
+        }
+    }
+}
