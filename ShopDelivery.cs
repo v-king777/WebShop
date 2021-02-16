9985af45cca0e7606a87599e73d7f9ac5107ad32
v-king777 <v.kolpashchikov@gmail.com>
2021-02-16 22:24:23 +0300

    new file:   ShopDelivery.cs

---
+++ ShopDelivery.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WebShop
+{
+    class ShopDelivery : Delivery
+    {
+        public ShopDelivery(string address) : base(address) { }
+
+        public override void DisplayAddress()
+        {
+            base.DisplayAddress();
+            Console.WriteLine("в магазин по адресу: " + Address);
+        }
+    }
+}
