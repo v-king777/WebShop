4b7c48a3fe5ce9d2b043930c8ca3c4ea2045643a
v-king777 <v.kolpashchikov@gmail.com>
2021-02-16 22:23:14 +0300

    modified:   PickPoint.cs

--- PickPoint.cs
+++ PickPoint.cs
@@ -8,28 +8,28 @@ namespace WebShop
     {
         protected string name;
         protected string address;
-        
+
         public PickPoint(string name, string address)
         {
             this.name = name;
             this.address = address;
         }
-        
+
         public string Name
         {
             get { return name; }
         }
-        
+
         public string Address
         {
             get { return address; }
         }
-        
+
         public virtual void Print()
         {
             Console.WriteLine("\n\t< Информация о точке доставки >\n");
-            Console.WriteLine("Name: " + name);
-            Console.WriteLine("Address: " + address);
+            Console.WriteLine("Название: " + name);
+            Console.WriteLine("Адрес: " + address);
         }
     }
 }
